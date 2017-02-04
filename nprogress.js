e41edadf28675d1395616c40470a529e06746808
Ger Hobbelt <ger@hobbelt.com>
2017-02-04 14:30:49 +0100

    tweak the transition points in the increment curve related to the configured holdOff - which may be large(ish) and otherwise 'damage' the progress speed curve at large.

--- nprogress.js
+++ nprogress.js
@@ -609,14 +609,14 @@
         var lim = Settings.incMaxRate;      // minimum growth rate
         var top = 1 - 0.01;                 // absolute maximum growth rate
         var rnd = Math.random() * lim;
-        if (d < 0.2) {
+        if (d < 0.2 * (1 - Settings.topHoldOff)) {
           // Start out between 3 - 6% increments --> 50-100% of the configured incMaxRate:
           lim /= 2;
-        } else if (d < 0.5) {
+        } else if (d < 0.5 * (1 - Settings.topHoldOff)) {
           // increment between 0 - 3% --> 5-50% of the configured incMaxRate:
           lim /= 20;
           rnd /= 2;
-        } else if (d < 0.8) {
+        } else if (d < 0.8 * (1 - Settings.topHoldOff)) {
           // increment between 0 - 2% --> 3-33% of the configured incMaxRate:
           lim /= 33;
           rnd /= 3;
